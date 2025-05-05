80852a1c76eb1913591085cbb208e0c0c809aa3e
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-05 11:54:58 -0300

    Creando Provider

---
+++ react-router-tutorial/src/context/UsuarioProvider.jsx
@@ -0,0 +1,17 @@
+import { UsuarioContext } from "./UsuarioContext"
+
+const usuario = {
+    nombre: 'jarias',
+    tecnologia: 'React',
+    mail: 'jarias@gmail.com',
+    redes: '@jarias'
+}
+export const UsuarioProvider = ({children}) =>{
+    return (
+        //Se provee de la infromación del usuario
+        //a todos los children
+        <UsuarioContext.Provider value={{usuario}}>
+
+        </UsuarioContext.Provider>
+    )
+}
