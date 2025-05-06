1002c5bbd80d076da42294dcd43df9cce8584514
Jose Arias <josee.ariasmelo@gmail.com>
2025-05-06 09:33:50 -0300

    Consumiendo la información del useContex en los componentes hijos

--- react-router-tutorial/src/routes/HomeScreem.jsx
+++ react-router-tutorial/src/routes/HomeScreem.jsx
@@ -1,36 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {UsuarioContext} from '../context/UsuarioContext'
 
 const HomeScreem = () => {
+  const {usuario} = useContext(UsuarioContext)
   return (
     <>
       <table className="table table-striped">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Tecnologia</th>
+            <th scope="col">Email</th>
+            <th scope="col">Redes</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+            <td>{usuario.nombre}</td>
+            <td>{usuario.tecnologia}</td>
+            <td>{usuario.mail}</td>
+            <td>{usuario.redes}</td>
+          </tr>         
         </tbody>
       </table>
     </>
